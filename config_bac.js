88ea217fef56742a630bdbb88a60c323ecf602a2
fish <353740902@qq.com>
2020-12-11 21:15:50 +0800

    配置化 up 主

---
+++ config_bac.js
@@ -0,0 +1,21 @@
+exports.ups = [
+  {
+    'type': 1,
+    'userID': 7005405,
+    'username': '海猫umi',
+    'groupQQ': 1109738985,
+  },
+  {
+    'type': 1,
+    'userID': 36782454,
+    'username': '可可啦',
+    'groupQQ': 577202845,
+  },
+  {
+    'type': 2,
+    'userID': 5339362,
+    'liveRoomID': 5339362,
+    'username': '女宅',
+    'groupQQ': 1577034,
+  },
+]
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const CQHttp = require('cqhttp')
 const Acfun = require('./lib/Acfun')
 const Bilibili = require('./lib/Bilibili')
-const { timingTask } = require('./lib/Utils')
+const { findIndexByAttr, timingTask } = require('./lib/Utils')
 const nedb = require('./lib/NedbConnection')
+
+let { ups } = require('./config')
 
 // 文档地址：https://github.com/cqmoe/cqhttp-node-sdk
 
@@ -10,64 +12,72 @@ const bot = new CQHttp({
   apiRoot: 'http://10.96.152.125:5700/',  // TODO: 待配置
 })
 
-const QQ = 3431290005
+const QQ = 3431290005  // self
 
+// QQ 消息类型
+const QQ_MESSAGE_TYPE_PRIVATE = 'private'
+const QQ_MESSAGE_TYPE_GROUP = 'group'
+
+// 直播平台类型
 const LIVE_TYPE_AC = 1
 const LIVE_TYPE_BILIBILI = 2
 
-let ups = [
-  {
-    'type': LIVE_TYPE_AC,
-    'userID': 7005405,
-    'username': '海猫umi',
-    'groupQQ': 1109738985,
-  },
-  {
-    'type': LIVE_TYPE_AC,
-    'userID': 36782454,
-    'username': '可可啦',
-    'groupQQ': 577202845,
-  },
-  {
-    'type': LIVE_TYPE_BILIBILI,
-    'liveRoomID': 5339362,
-    'username': '女宅',
-    'groupQQ': 1577034,
-  },
-]
-
-/*
-let message = `[CQ:at,qq=836869045] \n`
-message += 'test'
-bot('send_group_msg', {
-  group_id: GROUP_ID,
-  message: message,
-})
-*/
+// 避免配置错误，默认为空
+ups = ups || []
 
 bot.on('message',async context => {
-  console.log(context)
-  if (isAtMe(context.message) && context.message.search(/直播情况|((在|开|直)播.*(吗|没|？|\?))/i) !== -1) {
-    let body = await Bilibili.getLiveInfo(5339362)
+  console.log(context.message)
+
+  if (context.message_type === QQ_MESSAGE_TYPE_GROUP
+      && isAtMe(context.message) && context.message.search(/直播情况|((在|开|直)播.*(吗|没|？|\?))/i) !== -1) {
+    let groupQQ = context.group_id
+    let index = findIndexByAttr('groupQQ', groupQQ, ups)
+    if (index === -1) {
+      // 若为非直播相关 QQ 群，则不进行提醒
+      return
+    }
+    let up = ups[index]
     let message = ''
-    if (!checkBody(body)) {
-      message = '[ERROR]小助手好像生病了，请及时治疗'
-    } else if (body.data.room_info.live_status === 1) {
-      message = `直播放送中，点击进入直播间：https://live.bilibili.com/${5339362}`
-    } else if (body.data.room_info.live_status === 2) {
-      message = `正在回放中，点击进入直播间：https://live.bilibili.com/${5339362}`
+    if (up.type === LIVE_TYPE_AC) {
+      let userinfo = await Acfun.getLiveUserinfo(up.userID)
+      if (!userinfo) {
+        message = '[ERROR]小助手好像生病了，请及时治疗'
+      } else if (userinfo.liveId === undefined) {
+        message = '当前未直播，晚点再来看看吧~'
+      } else {
+        message = `正在回放中，点击进入直播间：https://live.acfun.cn/live/${up.userID}`
+      }
     } else {
-      message = '当前未直播，晚点再来看看吧~'
+      // 检测 B站主播开播状态
+      let body = await Bilibili.getLiveInfo(up.liveRoomID)
+      if (!checkBody(body)) {
+        message = '[ERROR]小助手好像生病了，请及时治疗'
+      } else if (body.data.room_info.live_status === 1) {
+        message = `直播放送中，点击进入直播间：https://live.bilibili.com/${5339362}`
+      } else if (body.data.room_info.live_status === 2) {
+        message = `正在回放中，点击进入直播间：https://live.bilibili.com/${5339362}`
+      } else {
+        message = '当前未直播，晚点再来看看吧~'
+      }
     }
     bot('send_group_msg', {
-      group_id: 1577034,
+      group_id: groupQQ,
       message: message,
     })
   }
-  if (context.message == '[CQ:at,qq=3431290005] 晚上好'){
-    let message = `你也好~`
+  if (isAtMe(context.message)) {
+    let groupQQ = context.group_id
+    let message = `不要打扰我工作啦~`
     bot('send_group_msg', {
-      group_id: 1577034,
+      group_id: groupQQ,
+      message: message,
+    })
+  }
+  if (context.message_type === QQ_MESSAGE_TYPE_PRIVATE && context.message === '233') {
+    console.log('test' . context.message)
+    let message = `success`
+    bot('send_group_msg', {
+      group_id: context.sender.user_id,
       message: message,
     })
   }
@@ -85,9 +95,14 @@ function checkBody(body) {
   return true
 }
 // 每 30 秒检测是否开播
+if (ups.length > 0) {
+  console.log('开始开播检测')
+} else {
+  console.log('未配置需要检测的直播 up')
+}
+
 timingTask(checkTask, 30)
 async function checkTask() {
-  console.log('开始开播检测')
   for (let up of ups) {
     if (up.type === LIVE_TYPE_AC) {
       await acLiveCheck(up.userID, up.groupQQ)
@@ -172,3 +187,39 @@ async function biliLiveCheck(liveRoomID, groupQQ, UPName) {
 
 bot.listen(5701)
 console.log('qqbot start in 5701')
+/*{ font: 0,
+  message: '哈哈哈',
+  message_id: 243355940,
+  message_type: 'private',
+  post_type: 'message',
+  raw_message: '哈哈哈',
+  self_id: 3431290005,
+  sender:
+  { age: 0, nickname: '‎', sex: 'unknown', user_id: 353740902 },
+  sub_type: 'friend',
+    time: 1607620570,
+  user_id: 353740902 }
+开始开播检测
+开始开播检测
+{ anonymous: null,
+  font: 0,
+  group_id: 1577034,
+  message: '[CQ:at,qq=3431290005] 开播了吗',
+  message_id: 1721027800,
+  message_type: 'group',
+  post_type: 'message',
+  raw_message: '[CQ:at,qq=3431290005] 开播了吗',
+  self_id: 3431290005,
+  sender:
+  { age: 0,
+    area: '',
+    card: '',
+    level: '',
+    nickname: '‎',
+    role: 'member',
+    sex: 'unknown',
+    title: '',
+    user_id: 353740902 },
+  sub_type: 'normal',
+    time: 1607620615,
+  user_id: 353740902 }*/
